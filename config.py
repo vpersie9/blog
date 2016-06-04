f3110c3bae2dd78876f18534336325321eda202c
vpersie9 <834538028@qq.com>
2016-06-04 17:09:29 +0800

    update a5

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 __author__ = 'vpersie9'
 class Config(object):
 
-    SECRET_KEY='z\xe7\x14\x0b\xdf*7|#\xbdPU:\xfb\x03\x0bWR\xb3_\xeb,'
+    SECRET_KEY='***'
     SQLALCHEMY_COMMIT_ON_TEARDOWN=True
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-    FACTORY_MAIL_SUBJECT_PREFIX='[Factory]'
-    FACTORY_MAIL_SENDER='Factory Admin <zengxiang7@163.com>'
-    FACTORY_ADMIN='zengxiang7@163.com'
+    FACTORY_MAIL_SUBJECT_PREFIX='***'
+    FACTORY_MAIL_SENDER='***'
+    FACTORY_ADMIN='***'
     FACTORY_POSTS_PER_PAGE = 20
     FACTORY_SLOW_DB_QUERY_TIME = 0.5
     SQLALCHEMY_RECORD_QUERIES = True
@@ -24,20 +24,20 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG=True
-    MAIL_SERVER='smtp.163.com'
+    MAIL_SERVER='smtp.**.com'
     MAIL_PORT=465
     MAIL_USE_TLS=False
     MAIL_USE_SSL=True
-    MAIL_USERNAME='zengxiang7@163.com'
-    MAIL_PASSWORD='xiang521jing'
-    SQLALCHEMY_DATABASE_URI="mysql://root:199099@localhost/develop?charset=utf8"
+    MAIL_USERNAME='***'
+    MAIL_PASSWORD='***'
+    SQLALCHEMY_DATABASE_URI="mysql://root:***@localhost/develop?charset=utf8"
 
 class TestingConfig(Config):
     TESTING=True
-    SQLALCHEMY_DATABASE_URI="mysql://root:199099@localhost/test?charset=utf8"
+    SQLALCHEMY_DATABASE_URI="mysql://root:***@localhost/test?charset=utf8"
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI="mysql://root:199099@localhost/production?charset=utf8"
+    SQLALCHEMY_DATABASE_URI="mysql://root:***@localhost/production?charset=utf8"
 
 config={
     'development':DevelopmentConfig,
